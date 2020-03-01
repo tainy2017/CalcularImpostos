de750f6c743d1f964cdec9e81cc751f74a85319b
Magda Amaral <magdaamaral2014@yahoo.com.br>
2020-03-01 20:27:48 -0300

    Alterei a estrutura de destino para framework 4.5. para que ele fosse compatível com ZipArchive. Foram criados três métodos: um proveniente do botão BtnBuscarNfe, outro que lê o arquivo zip e chama getDados. Ainda não está funcionando corretamente.

--- Codigo/CalculaImposto/CalculaImpostoForm.cs
+++ Codigo/CalculaImposto/CalculaImpostoForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,16 +12,83 @@ namespace CalculaImposto
 {
     public partial class FrmCalculaImposto : Form
     {
+        String caminho;
         public FrmCalculaImposto()
         {
             InitializeComponent();
         }
 
-        private void btnBuscar_Click(object sender, EventArgs e)
+        private void BtnBuscar_Click(object sender, EventArgs e)
         {
             if (openFileDialogNfe.ShowDialog() == DialogResult.OK)
             {
                 textBoxFile.Text = openFileDialogNfe.FileName;
+            }
+        }
+
+        private void BtnBuscarNfe_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Arquivo zip | *.zip";
+            openFileDialog.DefaultExt = "zip";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog.FileName;
+                caminho = textBox1.Text;
+
+                if (string.IsNullOrEmpty(openFileDialog.FileName) == false)
+                {
+                    try
+                    {
+
+                        LerZipEExibirGrid(caminho);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(String.Format("Não foi possível abrir o arquivo. Erro: {0}", ex.Message), "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void LerZipEExibirGrid(string caminho)
+        {
+            try
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(caminho))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        GetDados();
+                    }
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("Não foi possível abrir o arquivo. Erro: {0}", ex.Message), "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void GetDados()
+        {
+            try
+            {
+                DataSet dsResultado = new DataSet();
+                dsResultado.ReadXml(caminho);
+
+                if (dsResultado.Tables.Count != 0)
+                {
+                    if (dsResultado.Tables[0].Rows.Count > 0)
+                    {
+                        dataGridView1.DataSource = dsResultado.Tables[0];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
     }
